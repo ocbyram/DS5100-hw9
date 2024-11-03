0256d5a5ff86067a8507fe5b0b555be1c7d91ce8
ocbyram <ocb3wv@virginia.edu>
2024-11-03 16:57:56 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BookLover',
+    version='0.1',
+    description='A package that performs functions about book lists',
+    url='https://github.com/ocbyram/DS5100-hw9',
+    author='Olivia Byram',
+    author_email='ocb3wv@virginia.edu',
+    license='MIT',
+    packages=['BookLover']
+)
